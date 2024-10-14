ca94e3a7eb00a961daf6614636b0e532b086f785
Tatu Romero <tatiana.romero@ucuenca.edu.ec>
2024-10-14 17:01:01 -0500

    Modificación del código

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-  background(220);
+  background(244, 208, 63);
+  fill(27, 79, 114);
+  noStroke();
+  // square (20,20, 25)
+  for (let x = 10; x < width; x += 10)
+    // // circle(x,20, 10)
+    for (let y = 10; y < height; y += 10)
+      // circle(x,y,10)
+      square(x, y, random(2, 10));
 }
